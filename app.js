1fc8f8af9fdd7f2ceba9daf1e1edbbae55e869de
Jeremy Adams <mrjeremy@Jeremys-MacBook-Pro.local>
2020-08-14 13:49:06 -0700

    new form button

--- app.js
+++ app.js
@@ -22,6 +22,33 @@ jQuery(document).ready(function () {
         },
     });
 
+     // get the form elements defined in your form HTML above
+    
+     var form =     document.getElementById("my-form");
+     var button =   document.getElementById("btnContactUs");
+     var status =   document.getElementById("error_message");
+ 
+     // Success and Error functions for after the form is submitted
+     
+     function success() {
+       form.reset();
+    //    form.hide();
+       jQuery('form#my-form').hide();
+       button.style = "display: none ";
+       status.innerHTML = "Thanks!";
+     }
+ 
+     function error() {
+       status.innerHTML = "Oops! There was a problem.";
+     }
+ 
+     // handle the form submission event
+ 
+     form.addEventListener("submit", function(ev) {
+       ev.preventDefault();
+       var data = new FormData(form);
+       ajax(form.method, form.action, data, success, error);
+     });
 
 });
 jQuery('#contact-btn').click(e => {
@@ -73,10 +100,25 @@ jQuery('#reused_form').submit(function (e) {
 
     jQuery.ajax({
         type: "POST",
-        url: '/api/contact',
+        url: $form.action,
         data: $form.serialize(),
         success: after_form_submitted,
         dataType: 'json'
     });
 
 });
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+}
