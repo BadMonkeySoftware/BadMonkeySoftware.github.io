c8273eb9c077dea16b38b8f0f0e8d0522dea1ea0
Jeremy Adams <mrjeremy@Jeremys-MacBook-Pro.local>
2020-08-14 13:57:21 -0700

    fixed error and success message

--- app.js
+++ app.js
@@ -24,23 +24,28 @@ jQuery(document).ready(function () {
 
      // get the form elements defined in your form HTML above
     
-     var form =     document.getElementById("my-form");
-     var button =   document.getElementById("btnContactUs");
-     var status =   document.getElementById("error_message");
+    var form =              document.getElementById("my-form");
+    var button =            document.getElementById("btnContactUs");
+    var status_error =      document.getElementById("error_message");
+    var status_success =    document.getElementById("success_message");
+
  
      // Success and Error functions for after the form is submitted
      
-     function success() {
-       form.reset();
-    //    form.hide();
-       jQuery('form#my-form').hide();
-       button.style = "display: none ";
-       status.innerHTML = "Thanks!";
-     }
+    function success() {
+        form.reset();
+        form.style = "display: none";
+        status_success.style = "display: block";
+        status_error.style = "display: none";
+        button.style = "display: none ";
+    //    status_error.innerHTML = "Thanks!";
+    }
  
-     function error() {
-       status.innerHTML = "Oops! There was a problem.";
-     }
+    function error() {
+        status_error.style = "display: block";
+        status_success.style = "display: none";
+        status_error.innerHTML = "Oops! There was a problem.";
+    }
  
      // handle the form submission event
  
@@ -51,62 +56,7 @@ jQuery(document).ready(function () {
      });
 
 });
-jQuery('#contact-btn').click(e => {
-    e.stopPropagation();
-    e.preventDefault();
-    alert("Hey There")
-})
 
-function after_form_submitted(data) {
-    if (data.result == 'success') {
-        jQuery('form#reused_form').hide();
-        jQuery('#success_message').show();
-        jQuery('#error_message').hide();
-    } else {
-        jQuery('#error_message').append('<ul></ul>');
-
-        jQuery.each(data.errors, function (key, val) {
-            jQuery('#error_message ul').append('<li>' + key + ':' + val + '</li>');
-        });
-        jQuery('#success_message').hide();
-        jQuery('#error_message').show();
-
-        //reverse the response on the button
-        jQuery('button[type="button"]', $form).each(function () {
-            $btn = jQuery(this);
-            label = $btn.prop('orig_label');
-            if (label) {
-                $btn.prop('type', 'submit');
-                $btn.text(label);
-                $btn.prop('orig_label', '');
-            }
-        });
-
-    } //else
-}
-//http://reusableforms.com/d/p3/jquery-popup-contact-form-with-validation
-jQuery('#reused_form').submit(function (e) {
-    e.preventDefault();
-
-    $form = jQuery(this);
-    //show some response on the button
-    jQuery('button[type="submit"]', $form).each(function () {
-        $btn = jQuery(this);
-        $btn.prop('type', 'button');
-        $btn.prop('orig_label', $btn.text());
-        $btn.text('Sending ...');
-    });
-
-
-    jQuery.ajax({
-        type: "POST",
-        url: $form.action,
-        data: $form.serialize(),
-        success: after_form_submitted,
-        dataType: 'json'
-    });
-
-});
 
 function ajax(method, url, data, success, error) {
     var xhr = new XMLHttpRequest();
